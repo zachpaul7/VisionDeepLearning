3fb53d15a85abdd49192c98f7a2a6a032ced39a0
GodZero <60610390+zachpaul7@users.noreply.github.com>
2023-03-15 11:04:29 +0900

    Add files via upload

--- Chap02/Chap02_5.py
+++ Chap02/Chap02_5.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 15 00:41:32 2023
+Created on Wed Mar 15 09:58:28 2023
 
 @author: zachp
 """
@@ -8,28 +8,28 @@ Created on Wed Mar 15 00:41:32 2023
 import cv2 as cv
 import sys
 
-cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+frames=[]
 
+cap = cv.VideoCapture(0, cv.CAP_DSHOW) # 카메라와 연결 시도
+gray = False
 if not cap.isOpened():
-    sys.exit('카메라 연결 실패')
+    sys.exit('카메라 연결 실패.')
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
+   ret, frame = cap.read() # 비디오를 구성하는 프레임 흭득
+   if not ret:
         print('프레임 획득에 실패하여 루프를 나갑니다.')
         break
-    
-    key = cv.waitKey(1)
-    
-    if key == ord('g'):
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow('Video Display Gray', gray)
-        
-    elif key == ord('c'):
-        cv.imshow('Video Display Color', frame)
-    
-    elif key == ord('q'):
-        break
+   if gray :
+       frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+   cv.imshow('Video display',frame)
+   key=cv.waitKey(1) # 1밀리초 동안 키보드 입력 기다림
+   if key==ord('q'): # 'q' 키 입력시 루프를 빠져나감
+      break
+   elif key == ord('g'): # 'g' 키 입력시 명암
+       gray = True
+   elif key == ord('c'): # 'c' 키 입력시 컬러
+       gray = False
 
-cap.release()
+cap.release() #카메라 연결 끊음
 cv.destroyAllWindows()
